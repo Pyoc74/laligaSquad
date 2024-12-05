5f06a1235f992278117744e03b18dd013bf547ac
cat <pjhpjy0704@naver.com>
2024-12-05 15:30:14 +0900

    추가 작업_v0.1

--- my_cat_laliga_squad_240910.sql
+++ my_cat_laliga_squad_240910.sql
@@ -3,7 +3,7 @@
 -- Host: localhost    Database: my_cat
 -- ------------------------------------------------------
 -- Server version	8.0.33
-
+/* 메렁 메렁 */;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
